91f537af6552c11e8172be07ca82ad1297b2d36a
robertwenquan <robert.wen@nyu.edu>
2015-04-12 19:05:44 -0400

    add half of the graph4

---
+++ plot4.R
@@ -0,0 +1,47 @@
+#!/usr/bin/R -f
+
+library(dplyr)
+
+data <- read.table(file='data/household_power_consumption.txt', sep=';', header=TRUE)
+
+newdata <- mutate(data, Date = as.Date(Date, '%d/%m/%Y'))
+
+filtered <- filter(newdata, Date == '2007-02-01' | Date == '2007-02-02')
+
+# set drawing device
+png(filename='plot4.png', width=480, height=480)
+
+# multiple plot
+par(mfcol=c(2,2))
+
+# data for graph1
+selected <- mutate(filtered, Date = paste(as.character(Date), Time), Global_active_power=as.numeric(as.character(Global_active_power))) %>% select(Date, Global_active_power)
+x1 <- strptime(selected$Date, "%Y-%m-%d %H:%M:%S")
+x2 <- selected$Global_active_power
+
+# plot sub graph1
+plot(type='l', x1, x2, xlab = '', ylab = 'Global Active Power')
+
+# data for graph2
+selected2 <- mutate(filtered, Date = paste(as.character(Date), Time), Sub_metering_1=as.numeric(as.character(Sub_metering_1)), Sub_metering_2=as.numeric(as.character(Sub_metering_2)), Sub_metering_3=as.numeric(as.character(Sub_metering_3))) %>% select(Date, Sub_metering_1, Sub_metering_2, Sub_metering_3)
+
+x1 <- strptime(selected2$Date, "%Y-%m-%d %H:%M:%S")
+s1 <- selected2$Sub_metering_1
+s2 <- selected2$Sub_metering_2
+s3 <- selected2$Sub_metering_3
+
+# plot sub graph2
+plot(type = 'l', x1, s1, col='black', xlab = '', ylab = 'Energy sub metering')
+lines(x1, s2, col='red')
+lines(x1, s3, col='blue')
+legend("topright", pch = '__', col = c('black', 'red', 'blue'), legend = c('Sub_metering_1', 'Sub_metering_2', 'Sub_metering_3'))
+
+# data for graph3
+# plot graph3
+
+# data for graph4
+# plot graph4
+
+# close the device
+dev.off()
+
